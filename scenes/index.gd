3cf92ae454745ed4e3bec81ca36e222012629565
thiagola92 <thiagola92@gmail.com>
2026-06-25 18:58:38 -0300

    Click change topic and smooth move between years

--- scenes/index.gd
+++ scenes/index.gd
@@ -7,3 +7,17 @@ func _on_profile_pressed() -> void:
 
 func _on_timeline_item_rect_changed() -> void:
 	%Timeline.margins = %Profile.position.x + 32
+
+
+func _on_timeline_dir_selected(directory: String) -> void:
+	var dir := DirAccess.open(directory)
+	
+	assert(dir != null)
+	
+	var files := dir.get_files()
+	
+	for filename in files:
+		var filepath := directory.path_join(filename)
+		var project := load(filepath) as Project
+		
+		$ProjectDescription.project = project
--- scenes/timeline/timeline.gd
+++ scenes/timeline/timeline.gd
@@ -2,6 +2,10 @@
 extends ScrollContainer
 
 
+signal dir_selected(directory: String)
+
+const PROJECTS_DIR := "res://projects"
+
 const YearButtonScene := preload("res://scenes/timeline/year_button/year_button.tscn")
 
 const YearLinkScene := preload("res://scenes/timeline/year_link/year_link.tscn")
@@ -14,12 +18,18 @@ const SCROLL_CHECK = 10
 		%MarginContainer.add_theme_constant_override("margin_left", margins)
 		%MarginContainer.add_theme_constant_override("margin_right", margins)
 
-var _ignore_next_click: bool = false
+var _is_dragging: bool = false
+
+var _scroll_target: int = 0
 
 
 func _ready() -> void:
 	_clear()
 	_fill()
+
+
+func _process(_delta: float) -> void:
+	_move_to_target()
 
 
 func _clear() -> void:
@@ -28,7 +38,7 @@ func _clear() -> void:
 
 
 func _fill() -> void:
-	var dir := DirAccess.open("res://projects")
+	var dir := DirAccess.open(PROJECTS_DIR)
 	
 	assert(dir != null)
 	
@@ -37,9 +47,10 @@ func _fill() -> void:
 	dirs.reverse()
 	
 	for dirname in dirs:
+		var d := PROJECTS_DIR.path_join(dirname)
 		var b := YearButtonScene.instantiate() as YearButton
 		b.text = dirname
-		b.pressed.connect(_on_year_button_pressed.bind(b))
+		b.pressed.connect(_on_year_button_pressed.bind(b, d))
 		%Years.add_child(b)
 		
 		if dirname != dirs[-1]:
@@ -47,19 +58,33 @@ func _fill() -> void:
 			%Years.add_child(l)
 
 
+func _move_to_target() -> void:
+	if scroll_horizontal == _scroll_target:
+		return
+	
+	if abs(scroll_horizontal - _scroll_target) < 2:
+		scroll_horizontal = _scroll_target
+		return
+	
+	scroll_horizontal = int(lerp(scroll_horizontal, _scroll_target, 0.1))
+	
+
 func _on_gui_input(event: InputEvent) -> void:
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		if event is InputEventMouseMotion:
-			# Disable next button click if the user is scrolling with enthusiasm.
-			_ignore_next_click = _ignore_next_click or abs(event.relative.x) > SCROLL_CHECK
-			
+			_is_dragging = _is_dragging or abs(event.relative.x) > SCROLL_CHECK
 			scroll_horizontal -= event.relative.x
+			_scroll_target = scroll_horizontal
+	else:
+		_is_dragging = false
 
 
-func _on_year_button_pressed(button: YearButton) -> void:
-	# Igore this button press because the user is probably just scrolling.
-	if _ignore_next_click:
-		_ignore_next_click = false
+func _on_year_button_pressed(button: YearButton, directory: String) -> void:
+	# Ignore this button press because it's just the user dragging.
+	if _is_dragging:
+		_is_dragging = false
 		return
 	
-	scroll_horizontal = int(button.position.x)
+	_scroll_target = int(button.position.x)
+	
+	dir_selected.emit(directory)
